00fd74c08dd46f2f54511219517ed5ecf097fe14
pabloju23 <pablo.ju17@gmail.com>
2024-02-21 15:47:22 +0100

    hello_world full

--- 2_4/hello_world.cu
+++ 2_4/hello_world.cu
@@ -1,12 +1,17 @@
-#include <stdio.h> 
+#include <stdio.h>
 
-.... void mykernel( void ) {
-
+__global__ void mykernel( void ) {
+    // Kernel code goes here
+    printf("Hello, world from GPU!\n");
 }
 
-int main( int argc, char *argv[] ) {  
-  mykernel .... ();
-  printf("Hello, world!\n");  
-  return 0;
-}
+int main( int argc, char *argv[] ) {
+    // Launch kernel on GPU
+    mykernel<<<1, 1>>>();
+    cudaDeviceSynchronize(); // Ensures all kernel executions are completed before continuing
 
+    // Print "Hello, world!" from CPU
+    printf("Hello, world from CPU!\n");
+
+    return 0;
+}
